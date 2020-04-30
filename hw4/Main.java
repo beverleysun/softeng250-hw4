0ed3c96a634554862b1f59c7603e8fedd82bb3ec
Beverley Sun <beverleysun0205@gmail.com>
2020-04-30 21:56:18 +1200

    added writing to file functionality

--- hw4/Main.java
+++ hw4/Main.java
@@ -1,6 +1,9 @@
 package hw4;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,16 +12,52 @@ public class Main {
     public static void main(String args[]) {
 
         Scanner fileNameScanner = new Scanner(System.in);
-        System.out.println("Enter your file name: ");
-        String fileName = fileNameScanner.nextLine();
 
+        System.out.println("Enter your input file name: ");
+        String inputFileName = fileNameScanner.nextLine();
+
+        System.out.println("Enter output file name (Cannot be an existing file name in the working directory): ");
+        String outputFileName = fileNameScanner.nextLine();
+        Printer printer;
+
+        // try create new output file
         try {
-            AdjacencyList adjacencyList = new AdjacencyList(fileName);
+            File outputFile = new File(outputFileName);
+            if (outputFile.createNewFile()) {
+                System.out.println("File created: " + outputFileName);
+
+                // try create a new file writer
+                try {
+                    FileWriter fileWriter = new FileWriter(outputFileName);
+                    printer = new Printer(fileWriter);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    printer = null;
+                }
+
+            } else {
+                System.out.println("File already exists.");
+                printer = null;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred with creating " + outputFileName);
+            e.printStackTrace();
+            printer = null;
+        }
+
+        // main execution for the homework, try creating adjacency list representation of graph
+        try {
+            AdjacencyList adjacencyList = new AdjacencyList(inputFileName);
+
             List<Integer> sameInOut = adjacencyList.sameInOut();
-            System.out.println(sameInOut);
+            printer.writeSameInOut(sameInOut);
 
         } catch (FileNotFoundException e) {
-            System.out.println("File not found");
+            System.out.println("Input file not found");
+        }
+
+        if (printer != null){
+            printer.closeOutputFile();
         }
     }
 }
---
+++ hw4/Printer.java
@@ -0,0 +1,40 @@
+package hw4;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class Printer {
+    FileWriter _fileWriter;
+
+    public Printer(FileWriter fileWriter) {
+        _fileWriter = fileWriter;
+    }
+
+
+    public void writeSameInOut(List<Integer> sameInOutList) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Integer integer: sameInOutList) {
+            stringBuilder.append(integer + " ");
+        }
+
+        String sameInOutStr = stringBuilder.toString();
+        try {
+            _fileWriter.write(sameInOutStr.trim());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void closeOutputFile () {
+        try {
+            _fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
