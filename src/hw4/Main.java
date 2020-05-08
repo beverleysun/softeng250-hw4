32a17cb229ef47fa3108b74324480e44d147ba39
Beverley Sun <beverleysun0205@gmail.com>
2020-05-09 00:41:20 +1200

    Added test functionality

--- src/hw4/Main.java
+++ src/hw4/Main.java
@@ -4,18 +4,42 @@ import java.util.Scanner;
 import java.util.List;
 
 public class Main {
-    public static void main(String args[]) {
-        Scanner fileNameScanner = new Scanner(System.in);
+    private String inputFileName;
+    private String outputFileName;
+    private static final int NUM_TESTS = 28;
+    private static final boolean TEST_MODE = true;
 
-        // Get input file name
-        System.out.println("Enter your input file name: ");
-        String inputFileName = fileNameScanner.nextLine();
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        // Test cases and Test.java are not provided in submission
+        // Run Main first, then run Test.java
+        // Some tests will appear to fail, but the program only outputs one cycle.
+        // It may not be the cycle that is expected.
+        if (TEST_MODE) {
+            for (int i = 1; i <= NUM_TESTS; i++) {
+                main.inputFileName = "my-tests\\test" + i + ".txt";
+                main.outputFileName = "my-tests\\test" + i + "_output.txt";
+                main.process();
+            }
+        } else {
+            Scanner fileNameScanner = new Scanner(System.in);
+
+            // Get input file name
+            System.out.println("Enter your input file name: ");
+            main.inputFileName = fileNameScanner.nextLine();
+
+            // Get output file name
+            System.out.println("Enter output file name (Cannot be an existing file name in the working directory): ");
+            main.outputFileName = fileNameScanner.nextLine();
+
+            main.process();
+        }
+    }
+
+    private void process() {
         AdjacencyList adjList = new AdjacencyList(inputFileName);
-
-        // Get output file name
-        System.out.println("Enter output file name (Cannot be an existing file name in the working directory): ");
-        String outputFileName = fileNameScanner.nextLine();
-        FileCreator outputFile= new FileCreator(outputFileName);
+        FileCreator outputFile = new FileCreator(outputFileName);
 
         // Get data
         List<Integer> sameInOut = adjList.sameInOut(); // Vertices with the same in and out degree
